04666d1906f4c6dc66153bbabb6799979bebb1ba
VISHNUPRABHAKARAN-7100 <vishnuprabha478@gmail.com>
2024-06-14 22:46:40 +0530

    feat: Add continuous internet connectivity checking

--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -1,6 +1,9 @@
+import 'package:dashloc_interview/services/check_internet/view/no_internet_screen.dart';
 import 'package:dashloc_interview/util/constant_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../services/check_internet/connectivity_services/connectivity_services.dart';
 import 'widgets/pokemon_card.dart';
 
 class Home extends StatelessWidget {
@@ -122,109 +125,115 @@ class Home extends StatelessWidget {
     final deviceScreenWidth = MediaQuery.sizeOf(context).width;
     final deviceScreenHeight = MediaQuery.sizeOf(context).height;
 
-    return SafeArea(
-      child: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Pokemon'),
-            centerTitle: true,
-          ),
-          body: Column(
-            children: [
-              const Divider(),
-              const TabBar(
-                indicatorSize: TabBarIndicatorSize.tab,
-                indicatorColor: ConstantColors.primaryColor,
-                indicator: UnderlineTabIndicator(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(15),
-                  ),
-                  borderSide: BorderSide(width: 5.0),
+    final connectivityStatus = Provider.of<ConnectivityStatus>(context);
+    return connectivityStatus == ConnectivityStatus.connected
+        ? SafeArea(
+            child: DefaultTabController(
+              length: 2,
+              child: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Pokemon'),
+                  centerTitle: true,
                 ),
-                tabs: [
-                  Tab(
-                    child: Text(
-                      'All Pokemons',
-                      style: TextStyle(
+                body: Column(
+                  children: [
+                    const Divider(),
+                    const TabBar(
+                      labelStyle: TextStyle(
                         fontSize: 18,
                         color: Colors.black,
                       ),
-                    ),
-                  ),
-                  Tab(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Favorites',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                          ),
+                      labelColor: Colors.black,
+                      indicatorSize: TabBarIndicatorSize.tab,
+                      indicator: UnderlineTabIndicator(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(15),
                         ),
-                        SizedBox(width: 5),
-                        CircleAvatar(
-                          radius: 13,
-                          backgroundColor: Colors.blue,
-                          child: Text(
-                            '1',
-                            style: TextStyle(color: Colors.white),
+                        borderSide: BorderSide(
+                          width: 5.0,
+                          color: ConstantColors.primaryColor,
+                        ),
+                      ),
+                      tabs: [
+                        Tab(
+                          text: 'All Pokemons',
+                        ),
+                        Tab(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Favorites',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              CircleAvatar(
+                                radius: 13,
+                                backgroundColor: Colors.blue,
+                                child: Text(
+                                  '1',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    ColoredBox(
-                      color: Colors.grey.shade200,
-                      child: GridView.builder(
-                        itemCount: pokemonList.length,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          mainAxisExtent: deviceScreenHeight * 0.25,
-                        ),
-                        itemBuilder: (context, index) => PokemonCard(
-                          id: pokemonList[index]["id"],
-                          name: pokemonList[index]["name"],
-                          type: pokemonList[index]["types"],
-                          image: pokemonList[index]["sprite"],
-                        ),
-                      ),
-                    ),
-                    GridView.count(
-                      crossAxisCount: 2,
-                      childAspectRatio:
-                          (deviceScreenWidth / deviceScreenHeight),
-                      controller: ScrollController(keepScrollOffset: false),
-                      shrinkWrap: true,
-                      children: widgetList.map((String value) {
-                        return Container(
-                          color: Colors.green,
-                          child: Center(
-                            child: Text(
-                              value,
-                              style: const TextStyle(
-                                fontSize: 50.0,
-                                color: Colors.white,
+                    Expanded(
+                      child: TabBarView(
+                        children: [
+                          ColoredBox(
+                            color: Colors.grey.shade200,
+                            child: GridView.builder(
+                              itemCount: pokemonList.length,
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3,
+                                mainAxisExtent: deviceScreenHeight * 0.25,
+                              ),
+                              itemBuilder: (context, index) => PokemonCard(
+                                id: pokemonList[index]["id"],
+                                name: pokemonList[index]["name"],
+                                type: pokemonList[index]["types"],
+                                image: pokemonList[index]["sprite"],
                               ),
                             ),
                           ),
-                        );
-                      }).toList(),
+                          GridView.count(
+                            crossAxisCount: 2,
+                            childAspectRatio:
+                                (deviceScreenWidth / deviceScreenHeight),
+                            controller:
+                                ScrollController(keepScrollOffset: false),
+                            shrinkWrap: true,
+                            children: widgetList.map((String value) {
+                              return Container(
+                                color: Colors.green,
+                                child: Center(
+                                  child: Text(
+                                    value,
+                                    style: const TextStyle(
+                                      fontSize: 50.0,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                          // const Text('data2'),
+                        ],
+                      ),
                     ),
-                    // const Text('data2'),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          )
+        : const NoInternetScreen();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:dashloc_interview/splash/view/splash.dart';
 import 'package:dashloc_interview/util/constant_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'services/check_internet/connectivity_services/connectivity_services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,40 +13,54 @@ void main() {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  // Object of the internet checker.
+  final ConnectivityService _connectivityService = ConnectivityService();
 
   @override
   Widget build(BuildContext context) {
-    return MediaQuery(
-      data: MediaQuery.of(context).copyWith(
-        textScaler: const TextScaler.linear(1),
-      ),
-      child: MaterialApp(
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          primarySwatch: MaterialColor(
-            ConstantColors.primaryColor.value,
-            const <int, Color>{
-              50: Color(0xFFE0E7FF),
-              100: Color(0xFFC7D3FF),
-              200: Color(0xFFA9BEFF),
-              300: Color(0xFF8BA0FF),
-              400: Color(0xFF6D82FF),
-              500: Color(0xFF3558CD),
-              600: Color(0xFF1C34A3),
-              700: Color(0xFF00107A),
-              800: Color(0xFF000756),
-              900: Color(0xFF000333),
-            },
-          ),
-          useMaterial3: false,
+    _connectivityService.initialize();
+
+    return MultiProvider(
+      providers: [
+        // Declaring the Stream provider for the internet checker.
+        StreamProvider<ConnectivityStatus>.value(
+          value: _connectivityService.connectionStatusController.stream,
+          initialData: ConnectivityStatus.disconnected,
         ),
-        debugShowCheckedModeBanner: false,
-        home: const Splash(),
+      ],
+      child: MediaQuery(
+        data: MediaQuery.of(context).copyWith(
+          textScaler: const TextScaler.linear(1),
+        ),
+        child: MaterialApp(
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            primarySwatch: MaterialColor(
+              ConstantColors.primaryColor.value,
+              const <int, Color>{
+                50: Color(0xFFE0E7FF),
+                100: Color(0xFFC7D3FF),
+                200: Color(0xFFA9BEFF),
+                300: Color(0xFF8BA0FF),
+                400: Color(0xFF6D82FF),
+                500: Color(0xFF3558CD),
+                600: Color(0xFF1C34A3),
+                700: Color(0xFF00107A),
+                800: Color(0xFF000756),
+                900: Color(0xFF000333),
+              },
+            ),
+            useMaterial3: false,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const Splash(),
+        ),
       ),
     );
   }
